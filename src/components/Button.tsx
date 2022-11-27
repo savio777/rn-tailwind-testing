8d3953e33bbea5cb0a6cad1f5e89bfefe6366565
Sávio <saviopf22@gmail.com>
2022-11-26 23:11:35 -0300

    styled components do native wind

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,17 @@
 import { Text, TouchableOpacity } from "react-native";
-import { styled } from "nativewind";
+import { StyledComponent } from "nativewind";
+// import { styled } from "nativewind";
 
-function Button() {
+function Button({ ...props }) {
   return (
-    <TouchableOpacity className="h-14 bg-green-500 rounded-md justify-center item-center p-3">
-      <Text className="text-white font-bold text-md">Entrar</Text>
-    </TouchableOpacity>
+    <StyledComponent component={TouchableOpacity} {...props}>
+      <TouchableOpacity className="h-14 bg-green-500 rounded-md justify-center items-center p-3">
+        <Text className="text-white font-bold text-md">Entrar</Text>
+      </TouchableOpacity>
+    </StyledComponent>
   );
 }
 
-export default styled(Button);
+// export default styled(Button);
+
+export default Button;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,18 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+
 import Button from "../components/Button";
 
 export default function Home() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
+    <View className="flex-1 items-center justify-center bg-black p-8">
       <Feather name="home" size={34} color={colors.green[500]} />
 
       <Text className="text-primary-main font-bold mt-4 text-2xl">
         Hello Tailwind on Native
       </Text>
 
-      <Button />
+      <Button className="w-full mt-10" />
 
       <StatusBar style="light" />
     </View>
